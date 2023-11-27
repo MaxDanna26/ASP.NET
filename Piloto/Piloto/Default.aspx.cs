eb22bf772a52e2dfd12b8a72bc52d37da9b0f2f7
MaxDanna26 <maximilianodanna@hotmail.com>
2023-11-27 17:57:58 +0100

    AddSession

--- Piloto/Piloto/Default.aspx.cs
+++ Piloto/Piloto/Default.aspx.cs
@@ -14,88 +14,101 @@ namespace Piloto
             private static List<char> letrasIncorrectas = new List<char>();
             private static int errores = 1;
 
-            protected void Page_Load(object sender, EventArgs e)
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
             {
-                if (!IsPostBack)
+                InicializarJuego();
+                MostrarPalabraSecreta();
+            }
+        }
+
+        protected void InicializarJuego()
+        {
+            Session["palabraSecreta"] = "ahorcado";
+            Session["letrasCorrectas"] = new List<char>();
+            Session["letrasIncorrectas"] = new List<char>();
+            Session["errores"] = 0;
+        }
+
+        protected void ActualizarImagen()
+        {
+            int errores = (int)Session["errores"];
+            string url = $"/IMG/{errores}.png";
+            Image1.ImageUrl = url;
+        }
+
+        protected void btnIntentar_Click(object sender, EventArgs e)
+        {
+            char letra = txtLetra.Text.ToLower()[0];
+
+            List<char> letrasCorrectas = (List<char>)Session["letrasCorrectas"];
+            List<char> letrasIncorrectas = (List<char>)Session["letrasIncorrectas"];
+            int errores = (int)Session["errores"];
+
+            if (!letrasCorrectas.Contains(letra))
+            {
+                letrasCorrectas.Add(letra);
+
+                // Verificar si la letra está en la palabra secreta
+                string palabraSecreta = (string)Session["palabraSecreta"];
+                if (palabraSecreta.Contains(letra))
                 {
                     MostrarPalabraSecreta();
-
-                    Session["palabraSecreta"] = "ahorcado";
-                    Session["letrasCorrectas"] = new List<char>();
-                    Session["letrasIncorrectas"] = new List<char>();
-                    Session["errores"] = 0;
-
-
-                  }
-            }
-            
-            protected void ActualizarImagen()
-            {
-                string url = $"/IMG/{errores}.png";
-                Image1.ImageUrl = url;
-
-            }
-            
-            protected void btnIntentar_Click(object sender, EventArgs e)
-            {
-                char letra = txtLetra.Text.ToLower()[0];
-
-                if (!letrasCorrectas.Contains(letra))
-                {
-                    letrasCorrectas.Add(letra);
-
-                    // Verificar si la letra está en la palabra secreta
-                    if (palabraSecreta.Contains(letra))
-                    {
-                        MostrarPalabraSecreta();
-                        lblMensaje.Text = "¡Letra correcta!";
-                    }
-                    else
-                    {
-                        lblMensaje.Text = "¡Letra incorrecta!";
-                        errores++;
-                        ActualizarImagen();
-                        letrasIncorrectas.Add(letra);
-                        lblLetrasWrong.Text = string.Join("-", letrasIncorrectas);
-                        if(errores == 5)
-                        {
-                            btnIntentar.Enabled = false;
-                            lblMensaje.Text = "Fuiste ahorcado!";
-                        }
-                    }
-
-                    // Verificar si el jugador ha adivinado la palabra
-                    if (!lblPalabra.Text.Contains("_"))
-                    {
-                        lblMensaje.Text = "¡Felicidades! ¡Has adivinado la palabra!";
-                        btnIntentar.Enabled = false;
-                    }
+                    lblMensaje.Text = "¡Letra correcta!";
                 }
                 else
                 {
-                    lblMensaje.Text = "Ya has intentado esa letra. Intenta con otra.";
+                    lblMensaje.Text = "¡Letra incorrecta!";
+                    errores++;
+                    Session["errores"] = errores;
+                    ActualizarImagen();
+                    letrasIncorrectas.Add(letra);
+                    Session["letrasIncorrectas"] = letrasIncorrectas;
+                    lblLetrasWrong.Text = string.Join("-", letrasIncorrectas);
+                    if (errores == 5)
+                    {
+                        btnIntentar.Enabled = false;
+                        lblMensaje.Text = "Fuiste ahorcado!";
+                    }
                 }
+
+                // Verificar si el jugador ha adivinado la palabra
+                string palabraMostrada = MostrarPalabraSecreta();
+                if (!palabraMostrada.Contains("_"))
+                {
+                    lblMensaje.Text = "¡Felicidades! ¡Has adivinado la palabra!";
+                    btnIntentar.Enabled = false;
+                }
+            }
+            else
+            {
+                lblMensaje.Text = "Ya has intentado esa letra. Intenta con otra.";
+            }
 
             txtLetra.Text = "";
-            }
+        }
 
-            private void MostrarPalabraSecreta()
+        private string MostrarPalabraSecreta()
+        {
+            string palabraMostrada = "";
+            string palabraSecreta = (string)Session["palabraSecreta"];
+            List<char> letrasCorrectas = (List<char>)Session["letrasCorrectas"];
+
+            foreach (char letra in palabraSecreta)
             {
-                string palabraMostrada = "";
-
-                foreach (char letra in palabraSecreta)
+                if (letrasCorrectas.Contains(letra))
                 {
-                    if (letrasCorrectas.Contains(letra))
-                    {
-                        palabraMostrada += letra + " ";
-                    }
-                    else
-                    {
-                        palabraMostrada += "_ ";
-                    }
+                    palabraMostrada += letra + " ";
                 }
-
-                lblPalabra.Text = palabraMostrada.Trim();
+                else
+                {
+                    palabraMostrada += "_ ";
+                }
             }
+
+            lblPalabra.Text = palabraMostrada.Trim();
+            return palabraMostrada;
+        }
         }
     }
